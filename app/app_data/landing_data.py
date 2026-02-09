78c095494c0bbc23a1980d49eefb854f05a97af7
Jesuscy <jesuscarrascoyel.mur@gmail.com>
2026-02-09 09:39:10 +0100

    Bring changes from develop branch, Kafka conectors & Docker files mods

--- app/app_data/landing_data.py
+++ app/app_data/landing_data.py
@@ -1,5 +1,4 @@
 import os
-
 from numpy import info
 from requests_cache import datetime, logger
 from pyspark.sql import SparkSession
@@ -7,8 +6,8 @@ from pyspark.sql.functions import col, from_json
 from pyspark.sql.types import StructType, StructField, StringType
 from dotenv import load_dotenv
 
-from app_utils.azure_uploader import upload_to_datalake
 
+#Script de procesamiento de datos con Spark, este es el que se incluye en el contenedor de Spark en /opt/spark/jobs/ .
 
 load_dotenv('.env')
 
@@ -17,7 +16,7 @@ def spark_conection():
 
   spark_app_name = os.getenv('SPARK_APP_NAME')
   spark_master = os.getenv('SPARK_MASTER_URL')
-  spark = SparkSession.builder.appName(spark_app_name).master(spark_master) \
+  spark = SparkSession.builder.appName(spark_app_name) \
   .config(f"spark.hadoop.fs.azure.account.auth.type.{os.getenv('STORAGE_ACCOUNT_NAME')}.dfs.core.windows.net", "OAuth") \
   .config(f"spark.hadoop.fs.azure.account.oauth.provider.type.{os.getenv('STORAGE_ACCOUNT_NAME')}.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider") \
   .config(f"spark.hadoop.fs.azure.account.oauth2.client.id.{os.getenv('STORAGE_ACCOUNT_NAME')}.dfs.core.windows.net", os.getenv("CLIENT_ID")) \
@@ -34,7 +33,7 @@ def streaming_writing():
     .readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", os.getenv('KAFKA_BOOTSTRAP_SERVERS')) \
-    .option("subscribe", "positions") \
+    .option("subscribe", "vehicle_positions") \
     .option("startingOffsets", "latest") \
     .load()
 
@@ -62,4 +61,5 @@ def streaming_writing():
   query.awaitTermination()
 
  
+streaming_writing()
 
